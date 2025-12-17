2e8a4adb21aa671d693e5e671f7cb9058bf12382
Jessica Olofsson <olofsson.jessica@hotmail.com>
2025-12-17 13:18:17 +0100

    add test for create and update in controller

--- review-service/src/test/java/se/yrgo/controller/ReviewControllerTest.java
+++ review-service/src/test/java/se/yrgo/controller/ReviewControllerTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.yrgo.domain.Review;
+import se.yrgo.exception.ReviewNotFoundException;
 import se.yrgo.rest.ReviewRestController;
 import se.yrgo.service.review.ReviewService;
 
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ReviewControllerTest {
@@ -33,7 +35,7 @@ public class ReviewControllerTest {
     }
 
     @Test
-    void updateReview() {
+    void updateReviewTest() {
         Long id = 10L;
 
         Review newReview = new Review();
@@ -46,6 +48,27 @@ public class ReviewControllerTest {
         controller.updateReview(id, newReview);
 
         verify(service).updateReview(10L, newReview);
+    }
+
+    @Test
+    void couldNotUpdateTest() {
+        Long id = 10L;
+
+        Review newReview = new Review();
+        newReview.setProfileId(1L);
+        newReview.setText("Bad game");
+        newReview.setRating(2);
+        newReview.setGameId(1L);
+
+        doThrow(new ReviewNotFoundException("Couldn't find review"))
+                .when(service)
+                .updateReview(id, newReview);
+
+        ReviewRestController controller = new ReviewRestController(service);
+        assertThrows(
+                ReviewNotFoundException.class,
+                () -> controller.updateReview(id, newReview)
+        );
     }
 
 }
