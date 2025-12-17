820876c62802928e6820815aa1a1e779e100b269
Daniel <dangra0530@skola.goteborg.se>
2025-12-17 16:43:03 +0100

    fixt merge conflict

--- frontend/src/component/GameListComponent.tsx
+++ frontend/src/component/GameListComponent.tsx
@@ -1,27 +1,12 @@
+import type { ReactNode } from 'react';
 import './../style/Home.css'
-<<<<<<< HEAD
-import { type ReactNode } from 'react';
-=======
-import HeaderComponent from "../component/HeaderComponent.tsx";
-import type { Game } from '../interfaces/GameTypes.ts';
-import { getAllGames } from '../endpoints/GameEndpoints.ts';
-import ProfileComponent from '../component/ProfileComponent.tsx';
-import { useEffect, useState, type ReactElement, type ReactNode } from 'react';
-import GameComponent from './GameComponent.tsx';
->>>>>>> 0ece7151436105327de940bd52ad269bb87303a7
+
 
 interface Props {
     children: ReactNode;
 }
 
 export default function GameListComponent({ children }: Props) {
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> 0ece7151436105327de940bd52ad269bb87303a7
     return (
         <>
             <div className="games-section">
